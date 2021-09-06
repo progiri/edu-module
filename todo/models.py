55e5e10ef6e8a1ac487bfed19e54f97ce225e552
progiri <yerassyl.ak@mail.ru>
2021-09-06 17:08:21 +0600

    second commit

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.db.models.base import ModelStateFieldsCacheDescriptor
 from django.utils import timezone
 
 STATUS_CHOICES = [
--- todo_app/settings.py
+++ todo_app/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo',
-    'django_jsx'
-
+    'todo'
 ]
 
 MIDDLEWARE = [
